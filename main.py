5ed4bd906388e3e6f4b861c7b9d384621ace4260
prostod <102792901+prostod@users.noreply.github.com>
2022-04-01 17:14:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import cv2
 # функция обработки приложения
 def process_img(img, name, color):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
     img_mask = cv2.inRange(hsv_img, color[0], color[1])
-    cv2.imshow("mask" + name, img_mask)
+    
+    img_mixed = cv2.bitwise_and(img, img, mask = img_mask)
+    cv2.imshow("mask" + name, img_mixed)
 # функция выполняемая при изменении значения ползунков
 def update(value = 0):
     color_low = (
-    cv2.getTrackbarPos("h_min", "ui" )   
-    cv2.getTrackbarPos("s_min", "ui" )   
+    cv2.getTrackbarPos("h_min", "ui" ),   
+    cv2.getTrackbarPos("s_min", "ui" ),  
     cv2.getTrackbarPos("v_min", "ui" )   
     )
     color_high = (
-    cv2.getTrackbarPos("h_min", "ui" )   
-    cv2.getTrackbarPos("s_min", "ui" )   
-    cv2.getTrackbarPos("v_min", "ui" )   
+    cv2.getTrackbarPos("h_min", "ui" ),   
+    cv2.getTrackbarPos("s_min", "ui" ),   
+    cv2.getTrackbarPos("v_min", "ui" ) )
     color = (color_low, color_high)
-    )
+    process_img(img, 'img', color)
+    process_img(rainbow, 'rainbow', color)
+    
     
 img = cv2.imread("aquarium.jpg")
 rainbow = cv2.imread("2aquarium.jpg")
@@ -25,9 +30,9 @@ cv2.createTrackbar("h_min", "ui", 0, 180, update)
 cv2.createTrackbar("s_min", "ui", 0, 255, update)
 cv2.createTrackbar("v_min", "ui", 0, 255, update)
 
-cv2.createTrackbar("h_max", "ui", 0, 180, update)
-cv2.createTrackbar("s_max", "ui", 0, 255, update)
-cv2.createTrackbar("v_max", "ui", 0, 255, update)
+cv2.createTrackbar("h_max", "ui", 180, 180, update)
+cv2.createTrackbar("s_max", "ui", 255, 255, update)
+cv2.createTrackbar("v_max", "ui", 255, 255, update)
 cv2.imshow("ui", img)
 
 
